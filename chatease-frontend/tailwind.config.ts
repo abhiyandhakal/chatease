df1d0e40971970ef6a6699140266d90f81702b8f
Abhiyan Dhakal <itsabhiyandhakal@gmail.com>
2024-10-05 10:34:22 +0545

    update themes

--- chatease-frontend/tailwind.config.ts
+++ chatease-frontend/tailwind.config.ts
@@ -11,9 +11,11 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        "bg-secondary": "var(--bg-secondary)",
       },
     },
   },
   plugins: [],
+  darkMode: "class",
 };
 export default config;
