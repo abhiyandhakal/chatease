2c35ca9c66907217e996f92b82d444e8171d9993
Abhiyan Dhakal <itsabhiyandhakal@gmail.com>
2024-10-06 15:32:31 +0545

    Revert "update: remove postgres setup and use bun-default sqlite and drizzle"
    This reverts commit a67758c935fbd24b1020768989dec9e46f62ec5c.

--- chatease-backend/drizzle.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from "drizzle-kit";
-
-const config: Config = {
-  out: "./drizzle",
-  dialect: "sqlite",
-  schema: "./src/db/schema/**/*",
-};
-
-export default config;
--- chatease-backend/src/index.ts
+++ chatease-backend/src/index.ts
@@ -1,5 +1,34 @@
-import { db } from "./db";
-import { users } from "./db/schema/user";
+// import { Elysia, t } from "elysia";
+//
+// let chat: { id: string; message: string; time: number }[] = [];
+// const connectedClients: Set<any> = new Set();
+//
+// new Elysia()
+//   .ws("/ws", {
+//     open(ws) {
+//       connectedClients.add(ws);
+//       ws.send(JSON.stringify(chat));
+//     },
+//     message(ws, { message }) {
+//       const { id } = ws.data.query;
+//       const newMessage = { id, message, time: Date.now() };
+//       chat = [...chat, newMessage];
+//
+//       // Broadcast the updated chat to all connected clients
+//       for (const client of connectedClients) {
+//         client.send(JSON.stringify(chat));
+//       }
+//     },
+//     close(ws) {
+//       connectedClients.delete(ws);
+//     },
+//     query: t.Object({
+//       id: t.String(),
+//     }),
+//     body: t.Object({
+//       message: t.String(),
+//     }),
+//   })
+//   .listen(4000);
 
-const result = await db.select().from(users);
-console.log(result);
+console.log(process.env.POSTGRES_USER);
