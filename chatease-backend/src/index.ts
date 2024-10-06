293dae870746d0b7e06e8faba7d625a7731a3418
Abhiyan Dhakal <itsabhiyandhakal@gmail.com>
2024-10-06 14:37:13 +0545

    add: docker compose for postgres

--- chatease-backend/src/index.ts
+++ chatease-backend/src/index.ts
@@ -1,32 +1,34 @@
-import { Elysia, t } from "elysia";
+// import { Elysia, t } from "elysia";
+//
+// let chat: { id: string; message: string; time: number }[] = [];
+// const connectedClients: Set<any> = new Set();
+//
+// new Elysia()
+//   .ws("/ws", {
+//     open(ws) {
+//       connectedClients.add(ws);
+//       ws.send(JSON.stringify(chat));
+//     },
+//     message(ws, { message }) {
+//       const { id } = ws.data.query;
+//       const newMessage = { id, message, time: Date.now() };
+//       chat = [...chat, newMessage];
+//
+//       // Broadcast the updated chat to all connected clients
+//       for (const client of connectedClients) {
+//         client.send(JSON.stringify(chat));
+//       }
+//     },
+//     close(ws) {
+//       connectedClients.delete(ws);
+//     },
+//     query: t.Object({
+//       id: t.String(),
+//     }),
+//     body: t.Object({
+//       message: t.String(),
+//     }),
+//   })
+//   .listen(4000);
 
-let chat: { id: string; message: string; time: number }[] = [];
-const connectedClients: Set<any> = new Set();
-
-new Elysia()
-  .ws("/ws", {
-    open(ws) {
-      connectedClients.add(ws);
-      ws.send(JSON.stringify(chat));
-    },
-    message(ws, { message }) {
-      const { id } = ws.data.query;
-      const newMessage = { id, message, time: Date.now() };
-      chat = [...chat, newMessage];
-
-      // Broadcast the updated chat to all connected clients
-      for (const client of connectedClients) {
-        client.send(JSON.stringify(chat));
-      }
-    },
-    close(ws) {
-      connectedClients.delete(ws);
-    },
-    query: t.Object({
-      id: t.String(),
-    }),
-    body: t.Object({
-      message: t.String(),
-    }),
-  })
-  .listen(4000);
+console.log(process.env.POSTGRES_USER);
