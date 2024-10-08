94daa5cdf7f329b76363b370e7b530fe6fbed464
Abhiyan Dhakal <itsabhiyandhakal@gmail.com>
2024-10-08 08:12:18 +0545

    add: feat-auth: add login api

--- chatease-backend/src/services/auth.ts
+++ chatease-backend/src/services/auth.ts
@@ -21,10 +21,7 @@ class AuthService {
   }) {
     try {
       // Encrypt password
-      const encryptedPassword = jwt.sign(
-        { password },
-        process.env.JWT_SECRET || "",
-      );
+      const encryptedPassword = await Bun.password.hash(password);
 
       // Don't allow duplicate emails and usernames
       const existingUserWithUsername = await db
@@ -65,6 +62,57 @@ class AuthService {
         success: true,
         message: "User created successfully",
         user: result[0],
+      };
+    } catch (err) {
+      if (err instanceof Error) {
+        return error(500, {
+          success: false,
+          message: "Internal server error",
+          error: err.message,
+        });
+      }
+    }
+  }
+
+  async login({ username, password }: { username: string; password: string }) {
+    try {
+      // Check if user exists
+      const userList = await db
+        .select()
+        .from(users)
+        .where(eq(users.username, username));
+      const user = userList[0];
+      if (!user) {
+        return error(400, {
+          success: false,
+          message: "User not found",
+        });
+      }
+
+      // Verify password
+      if (!(await Bun.password.verify(password, user.password))) {
+        return error(400, {
+          success: false,
+          message: "Invalid password",
+        });
+      }
+
+      const token = jwt.sign(
+        {
+          username: user.username,
+          email: user.email,
+          profilePic: user.profilePic,
+        },
+        process.env.JWT_SECRET || "",
+        {
+          expiresIn: "1d",
+        },
+      );
+
+      return {
+        success: true,
+        message: "User logged in successfully",
+        token,
       };
     } catch (err) {
       if (err instanceof Error) {
