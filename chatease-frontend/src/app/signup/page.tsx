e8d761ec28f8a601f5456d2f093d5a6e7c8e27ee
Abhiyan Dhakal <itsabhiyandhakal@gmail.com>
2024-10-08 20:38:43 +0545

    update ui for signup text

--- chatease-frontend/src/app/signup/page.tsx
+++ chatease-frontend/src/app/signup/page.tsx
@@ -6,7 +6,7 @@ export default function Signup() {
     <main className="grid place-items-center w-full p-4">
       <Card className="bg-secondary border-none shadow-lg px-8 py-6 max-w-3xl rounded-2xl w-full">
         <CardHeader>
-          <CardTitle>Sign Up</CardTitle>
+          <CardTitle className="text-center text-4xl">Sign Up</CardTitle>
         </CardHeader>
         <CardContent className="w-full">
           <SignupForm />
