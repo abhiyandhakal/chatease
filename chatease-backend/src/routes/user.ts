d1e303def708ee76499262b0c5c4705e8d1abe6d
Abhiyan Dhakal <itsabhiyandhakal@gmail.com>
2024-10-10 19:18:53 +0545

    backend: add api for other user's detail

--- chatease-backend/src/routes/user.ts
+++ chatease-backend/src/routes/user.ts
@@ -1,18 +1,56 @@
-import { Elysia } from "elysia";
+import { Elysia, error, t } from "elysia";
 import authHeaderValidator, {
   authHeaderValidation,
 } from "../utils/auth-header-validator";
+import { db } from "../db";
+import { users } from "../db/schema/user";
+import { eq } from "drizzle-orm";
 
-const userRoute = new Elysia({ prefix: "/user" }).get(
-  "/profile",
-  async ({ headers }) => {
-    const res = authHeaderValidator(headers);
-    if (res.type === "error") {
-      return res.error;
-    }
-    return res.user;
-  },
-  authHeaderValidation,
-);
+const userRoute = new Elysia({ prefix: "/user" })
+  // Get your own profile
+  .get(
+    "/profile",
+    async ({ headers }) => {
+      const res = authHeaderValidator(headers);
+      if (res.type === "error") {
+        return res.error;
+      }
+      return res.user;
+    },
+    authHeaderValidation,
+  )
+  // Get another user's profile
+  .get(
+    "/:username",
+    async ({ params, headers }) => {
+      const res = authHeaderValidator(headers);
+      if (res.type === "error") {
+        return res.error;
+      }
+
+      const userArr = await db
+        .select()
+        .from(users)
+        .where(eq(users.username, params.username));
+
+      if (userArr.length === 0) {
+        return error(404, { success: false, message: "User not found" });
+      }
+
+      const user = userArr[0];
+      const publicUser: PublicUser = {
+        username: user.username,
+        fullName: user.fullName,
+        profilePic: user.profilePic,
+      };
+      return publicUser;
+    },
+    {
+      params: t.Object({
+        username: t.String(),
+      }),
+      ...authHeaderValidation,
+    },
+  );
 
 export default userRoute;
