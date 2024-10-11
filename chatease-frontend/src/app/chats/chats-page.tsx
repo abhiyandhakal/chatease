bb20e42a58c660ace18b2504d88b6a9ffa1d1dd9
Abhiyan Dhakal <itsabhiyandhakal@gmail.com>
2024-10-11 11:26:04 +0545

    set ssr to false for chats page

---
+++ chatease-frontend/src/app/chats/chats-page.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import Sidebar from "./sidebar";
+import twConfig from "tailwindcss/resolveConfig";
+import config from "../../../tailwind.config";
+
+export default function ChatsPage() {
+  const twConfigLg = twConfig(config).theme.screens.xl;
+  const twConfigLgInt = parseInt(twConfigLg.replace("px", ""));
+  const isMobile = window.innerWidth < twConfigLgInt;
+
+  return (
+    <ResizablePanelGroup direction="horizontal">
+      <ResizablePanel defaultSize={!isMobile ? 20 : undefined}>
+        <Sidebar />
+      </ResizablePanel>
+      <ResizableHandle withHandle className="w-1" />
+      <ResizablePanel></ResizablePanel>
+    </ResizablePanelGroup>
+  );
+}
--- chatease-frontend/src/app/chats/page.tsx
+++ chatease-frontend/src/app/chats/page.tsx
@@ -1,26 +1,9 @@
-"use client";
+import dynamic from "next/dynamic";
 
-import {
-  ResizableHandle,
-  ResizablePanel,
-  ResizablePanelGroup,
-} from "@/components/ui/resizable";
-import Sidebar from "./sidebar";
-import twConfig from "tailwindcss/resolveConfig";
-import config from "../../../tailwind.config";
+const DynamicComponentWithNoSSR = dynamic(() => import("./chats-page"), {
+  ssr: false,
+});
 
 export default function Page() {
-  const twConfigLg = twConfig(config).theme.screens.xl;
-  const twConfigLgInt = parseInt(twConfigLg.replace("px", ""));
-  const isMobile = window.innerWidth < twConfigLgInt;
-
-  return (
-    <ResizablePanelGroup direction="horizontal">
-      <ResizablePanel defaultSize={!isMobile ? 20 : undefined}>
-        <Sidebar />
-      </ResizablePanel>
-      <ResizableHandle withHandle className="w-1" />
-      <ResizablePanel></ResizablePanel>
-    </ResizablePanelGroup>
-  );
+  return <DynamicComponentWithNoSSR />;
 }
