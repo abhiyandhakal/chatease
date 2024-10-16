6ce40edf6cf523480b7b0cfa6e4609430eb1aa01
Abhiyan Dhakal <itsabhiyandhakal@gmail.com>
2024-10-17 01:35:49 +0545

    fix(backend): reset the database schema generated by drizzle

--- chatease-backend/drizzle/0005_spotty_christian_walker.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `dmChannel` (
-	`id` text PRIMARY KEY NOT NULL,
-	`senderId` text NOT NULL,
-	`receiverId` text NOT NULL,
-	FOREIGN KEY (`senderId`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action,
-	FOREIGN KEY (`receiverId`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action
-);
