0db00305e03d5710eca2c2f8a8575a1aa106e581
Abhiyan Dhakal <itsabhiyandhakal@gmail.com>
2025-01-02 21:54:59 +0545

    feat: keep the chat selected on refresh

--- chatease-frontend/src/store/chat.ts
+++ chatease-frontend/src/store/chat.ts
@@ -1,12 +1,17 @@
 import { getChatList } from "@/lib/api/chat";
-import { atom } from "jotai";
+import { atom, getDefaultStore } from "jotai";
 import { atomWithRefresh } from "jotai/utils";
+
+const store = getDefaultStore();
 
 export const chatListAtom = atomWithRefresh(async () => {
   const response = await getChatList();
   const data = response.data.data;
   return data as Chat[];
 });
+
+const url = new URL(window.location.href);
+const chatId = url.searchParams.get("chat");
 
 export const chatSelectedAtom = atom<Chat | null>(null);
 
@@ -16,3 +21,13 @@ export const chatMessagesAtom = atom<
     chatId: string;
   }[]
 >([]);
+
+if (chatId || chatId === "" || chatId === "null" || chatId === "undefined") {
+  // const chat = chatListAtom.find((chat) => chat.id === chatId);
+  // chatSelectedAtom.set(chat);
+  (async function () {
+    const chats = await store.get(chatListAtom);
+    const chat = chats.find((chat) => chat.id === chatId);
+    if (chat) store.set(chatSelectedAtom, chat);
+  })();
+}
